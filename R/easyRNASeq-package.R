882540d6b8a479b255277df5b1fb2c41636aeb8b
Nicolas Delhomme <delhomme@embl.de>
2014-04-03 07:06:45 +0000

    Corrected a typo in a function to get the names from a RangedData annotation.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/branches/RELEASE_2_13/madman/Rpacks/easyRNASeq@88359 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/easyRNASeq-package.R
+++ R/easyRNASeq-package.R
@@ -10,8 +10,8 @@
 ##' \tabular{ll}{
 ##' Package: \tab easyRNASeq\cr
 ##' Type: \tab Package\cr
-##' Version: \tab 1.8.7\cr
-##' Date: \tab 2014-03-25\cr
+##' Version: \tab 1.8.8\cr
+##' Date: \tab 2014-04-03\cr
 ##' License: \tab Artistic-2.0\cr
 ##' LazyLoad: \tab yes\cr
 ##' Depends: \tab methods, parallel, biomaRt, edgeR, DESeq, genomeIntervals, LSD, Rsamtools, ShortRead, RnaSeqTutorial\cr
