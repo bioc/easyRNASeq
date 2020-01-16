2303a09e048656db13e1118a62bd7893908ce742
nicolasDelhomme <nicolas.delhomme@slu.se>
2020-01-16 11:45:24 +0100

    Moved the vignette data retrieval to the biocFileCache

--- R/BiocFileCache.R
+++ R/BiocFileCache.R
@@ -1,27 +1,32 @@
-##' Manages the data necessary for the examples using BiocFileCache
-##'
-##' Manages the tutorial and example data using the
-##' \code{\linkS4class{BiocFileCache}} package
-##'
-##' @name BiocFileCache methods
-##' @rdname BiocFileCache-methods
-##' @aliases .get_cache .get_cache,ANY-method fetchData
-##' fetchData,character-method tutorialData tutorialData,ANY-method
-##' @docType methods
-##' @param fileURL The URL of the file to retrieve. Alternatively, the ID of the
-##' file in the BiocFileCache (i.e. the file basename), can be used.
-##' @param \dots unused for the time being
-##' @section Methods: \describe{
-##' \item{.get_cache}{internal function to set up the cache}
-##' \item{fetchData}{A function to fetch tutorial data, a file at a time}
-##' \item{tutorialData}{the function to retrieve all the tutorial data and
-##' cache it, if it is not already available}}
-##' @keywords methods
-##' @usage fetchData(fileURL)
-##' @seealso \code{\linkS4class{BiocFileCache}}
-##' @examples
-##' tdir <- tutorialData()
-##' gAnnot.path <- fetchData("gAnnot.rda")
+#' Manages the data necessary for the examples using BiocFileCache
+#'
+#' Manages the tutorial, example and vignette data using the
+#' \code{\linkS4class{BiocFileCache}} package
+#'
+#' @name BiocFileCache methods
+#' @rdname BiocFileCache-methods
+#' @aliases .get_cache .get_cache,ANY-method fetchData
+#' fetchData,character-method tutorialData tutorialData,ANY-method
+#' vignetteData vignetteData,ANY-method
+#' @docType methods
+#' @param fileURL The URL of the file to retrieve. Alternatively, the ID of the
+#' file in the BiocFileCache (i.e. the file basename), can be used.
+#' @param \dots unused for the time being
+#' @section Methods: \describe{
+#' \item{.get_cache}{internal function to set up the cache}
+#' \item{fetchData}{A function to fetch tutorial data, a file at a time}
+#' \item{tutorialData}{the function to retrieve all the tutorial data and
+#' cache it, if it is not already available}
+#' \item{vignetteData}{the function to retrieve all the tutorial data and
+#' cache it, if it is not already available}}
+#' @keywords methods
+#' @usage fetchData(fileURL)
+#' @seealso \code{\linkS4class{BiocFileCache}}
+#' @examples
+#' tdir <- tutorialData()
+#' gAnnot.path <- fetchData("gAnnot.rda")
+#' vdir <- vignetteData()
+#' md5.txt <- fetchData("md5.txt")
 setMethod(
   f=".get_cache",
   signature="ANY",
@@ -54,3 +59,10 @@ setMethod(
       return(dirname(tdat[1]))
     })
 
+setMethod(
+  f="vignetteData",
+  signature="ANY",
+  definition = function(...){
+    vdat <- sapply(VIGNETTE.DATA,fetchData)
+    return(dirname(vdat[1]))
+  })
